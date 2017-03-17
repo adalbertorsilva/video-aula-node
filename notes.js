f41e67264edc9e6fbfb761d2e6e196a2eeeb759a
adalberto.silva <adalberto.silva@concrete.com.br>
2017-03-17 17:52:01 -0300

    implementacao do metodo add com criacao de arquivo json

--- notes.js
+++ notes.js
@@ -7,10 +7,21 @@ var addNote = (title, body) => {
   var note = {
     title,
     body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
   }
 
-  notes.push(note);
-  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  var duplicateNotes = notes.filter((note) => note.title === title );
+
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
